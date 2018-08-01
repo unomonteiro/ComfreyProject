b315f9d6d2c6d8b521f8e03af18069c3dc6c1008
Carlos Monteiro <offbyte@gmail.com>
2018-08-01 13:45:49 +0100

    improve plantDetails UI
    improve plantDetailItem on missing image or title
    refactor styles
    fixed lists being clipped to padding

--- app/src/main/java/io/monteirodev/comfreyproject/ui/plants/PlantDetailsAdapter.java
+++ app/src/main/java/io/monteirodev/comfreyproject/ui/plants/PlantDetailsAdapter.java
@@ -2,7 +2,6 @@ package io.monteirodev.comfreyproject.ui.plants;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.text.Spanned;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,8 +19,8 @@ import io.monteirodev.comfreyproject.data.PlantDetail;
 import io.monteirodev.comfreyproject.utils.GlideApp;
 
 import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
 import static com.bumptech.glide.load.engine.DiskCacheStrategy.AUTOMATIC;
-import static io.monteirodev.comfreyproject.utils.UiUtils.fromHtml;
 
 public class PlantDetailsAdapter extends RecyclerView.Adapter<PlantDetailsAdapter.DetailViewHolder> {
 
@@ -42,16 +41,26 @@ public class PlantDetailsAdapter extends RecyclerView.Adapter<PlantDetailsAdapte
         if (TextUtils.isEmpty(detail.getImageUrl())) {
             holder.imageView.setVisibility(GONE);
         } else {
-            holder.imageView.setVisibility(View.VISIBLE);
+            holder.imageView.setVisibility(VISIBLE);
             GlideApp.with(holder.imageView.getContext())
                     .load(detail.getImageUrl())
                     .diskCacheStrategy(AUTOMATIC)
                     .into(holder.imageView);
         }
 
-        holder.titleTextView.setText(detail.getTitle());
+        if (TextUtils.isEmpty(detail.getTitle())) {
+            holder.titleTextView.setVisibility(GONE);
+        } else {
+            holder.titleTextView.setText(detail.getTitle());
+            holder.titleTextView.setVisibility(VISIBLE);
+        }
 
-        holder.bodyTextView.loadData(detail.getBody(), "text/html", null);
+        if (TextUtils.isEmpty(detail.getBody())) {
+            holder.bodyWebView.setVisibility(GONE);
+        } else {
+            holder.bodyWebView.setVisibility(VISIBLE);
+            holder.bodyWebView.loadData(detail.getBody(), "text/html", null);
+        }
     }
 
     @Override
@@ -73,7 +82,7 @@ public class PlantDetailsAdapter extends RecyclerView.Adapter<PlantDetailsAdapte
         @BindView(R.id.detail_title_text_view)
         TextView titleTextView;
         @BindView(R.id.detail_body_web_view)
-        WebView bodyTextView;
+        WebView bodyWebView;
 
         DetailViewHolder(View itemView) {
             super(itemView);
